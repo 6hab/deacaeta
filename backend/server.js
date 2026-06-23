ede6016a9858bb15c05dc09f6894d607a21c4069
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-06-23 23:49:46 +0200

    Modif du temps de fetch

--- backend/server.js
+++ backend/server.js
@@ -172,7 +172,7 @@ app.get("/coolsongs", async (req, res) => {
     try {
         const [row] = await pool.execute("SELECT last_fetch_time FROM cache_meta WHERE id = 1");
         
-        if (row.length === 0 || (Date.now() - row[0]?.last_fetch_time?.getTime() >= 1000 * 60 * 60 *2)) {
+        if (row.length === 0 || (Date.now() - row[0]?.last_fetch_time?.getTime() >= 1000 * 60 * 60 *12)) {
             await syncSongs();
             res.json(await getActiveCachedSongs());
         }
