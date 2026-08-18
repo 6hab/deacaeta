8e96c06408b3ba28e4770117faef07211334f1d0
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-08-18 23:59:22 +0200

    feat: revu de Song of the day

--- src/components/SongOfTheDay.tsx
+++ src/components/SongOfTheDay.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Sparkle, Play } from "lucide-react";
+import { Sparkle, User, Play } from "lucide-react";
 import { useSongOfTheDay } from "../hooks/home/useSongOfTheDay";
+import { Link } from "react-router-dom";
 
 export function SongOfTheDay() {
   const { song, loading, error } = useSongOfTheDay();
@@ -12,70 +13,81 @@ export function SongOfTheDay() {
 
   return (
     <div className="relative rounded-2xl overflow-hidden min-h-70">
-
-      <img
-        src={song.thumbnail}
-        alt=""
+      <img src={song.thumbnail} alt="" 
         className="absolute inset-0 w-full h-full object-cover blur scale-110"
       />
 
-      <div className="absolute inset-0 bg-black/30" />
+      <div className="absolute inset-0  bg-black/10" />
 
-      <div className="relative flex flex-col md:flex-row items-center gap-6 p-6">
-
+      <div className="relative flex flex-col md:flex-row items-center gap-6 p-5 mb-3">
         <div className="flex-1 min-w-0">
-          <span className="inline-block px-3 py-1 text-sm rounded-full bg-white/10 text-white mb-3">
-            <div className="flex items-center gap-1">
-              <Sparkle size={16} className=""/> 
+          
+          <span className="inline-block bg-amber-400/40 rounded-full px-3 py-1 text-sm mb-3">
+            <div className="flex text-white items-center gap-1">
+              <Sparkle size={16} className="text-amber-400"/>
               Song of the day
             </div>
           </span>
 
-          <p className="text-2xl font-bold text-white mb-2 line-clamp">{song.title}</p>
+          <p className="text-2xl font-bold text-white line-clamp mb-3">{song.title}</p>
 
-          <div className="flex gap-2 mb-3">
+          {song.artists.map((artist, index) => (
+            <span key={artist.artist_id} className="inline-flex items-center gap-2">
+              {index !== 0 && <span className="text-white/50 text-sm">feat.</span>}
+              
+              <Link 
+                to={`/artist/${artist.artist_id}`}
+                className="inline-flex items-center gap-2 hover:opacity-80"
+              >
+                {index === 0 && artist.photo ? 
+                <img
+                  src={artist.photo} 
+                  alt={artist.name_original} 
+                  className="w-7 h-7 rounded-full object-cover"
+                /> :
+                <User className="text-white/90 w-7 h-7 bg-white/60 rounded-full" />}
+                
+                <p className="text-sm text-white/70 ">{artist.name_original}</p>
+              </Link>
+            </span>
+          ))}
+
+          <div className="flex gap-2 mb-3 mt-3">
             {song.tags.map((tag) => (
-              <span key={tag} className="text-xs px-2 py-1 rounded-full bg-white/10 text-white/80">
+              <span key={tag} className="text-white/90 text-xs bg-white/20 rounded-full px-2 py-1">
                 {tag}
               </span>
             ))}
           </div>
 
-          {song.artists.map((artist) => (
-            <p key={artist.artist_id} className="text-sm text-white/80">
-              {artist.name_original}
-            </p>
-          ))}
-        </div>
-
-        <div className="w-full md:w-80 aspect-video rounded-lg overflow-hidden shrink-0 relative">
-          {isPlaying ? (
-            <iframe
-              className="w-full h-full"
-              src={`https://www.youtube.com/embed/${song.video_id}?autoplay=1`}
-              title={song.title}
-              allowFullScreen
-            />
-          ) : (
-            <button
-              onClick={() => setIsPlaying(true)}
-              className="relative w-full h-full"
-            >
-              <img
-                src={song.thumbnail}
-                alt=""
-                className="w-full h-full object-cover"
+          <div className="w-full md:w-80 aspect-video rounded-lg overflow-hidden shrink-0 relative">
+            {isPlaying ? (
+              <iframe
+                className="w-full h-full"
+                src={`https://www.youtube.com/embed/${song.video_id}?autoplay=1`}
+                title={song.title}
+                allow="autoplay; encrypted-media"
+                allowFullScreen
               />
-              <div className="absolute inset-0 flex items-center justify-center bg-black/10 hover:bg-black/20">
-                <div className="w-14 h-14 rounded-full bg-red-600/90 flex items-center justify-center">
-                  <Play size={22} className="fill-white" />
+            ) : (
+              <button onClick={() => setIsPlaying(true)} className="relative w-full h-full">
+                <img 
+                  src={song.thumbnail} 
+                  alt="" 
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute inset-0 flex items-center justify-center hover:bg-black/10">
+                  <div className="w-14 h-14 bg-mauve-600/90 rounded-full flex items-center justify-center">
+                    <Play size={22} className="text-white"/>
+                  </div>
                 </div>
-              </div>
-            </button>
-          )}
+              </button>
+            )}
+          </div>
         </div>
-
       </div>
+
     </div>
+  
   );
 }
