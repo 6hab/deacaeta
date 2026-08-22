00cca0fa63084ac1577b39ef7670371d6f231b04
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-08-22 14:38:12 +0200

    fix: correction d'un bug qui affiche les titres entre les musiques + un peu de style

--- src/components/RencentlyAdded.tsx
+++ src/components/RencentlyAdded.tsx
@@ -26,36 +26,35 @@ export function RecentlyAdded() {
   if (songs.length === 0) return null
 
   return (
-    <div className="p-3">
-      <div className="flex gap-2 text-white font-semibold mb-1 items-center">
+    <div className="py-8 px-2">
+      <div className="flex gap-2 text-white font-semibold mb-3 items-center">
         <Clock size={16} />
         Recently added
       </div>
       <div className="flex overflow-x-auto [&::-webkit-scrollbar]:hidden [scrollbar-none] gap-2">
         {songs.map((song) => (
-          <a
-            key={song.video_id}
-            href={`https://www.youtube.com/watch?v=${song.video_id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="shrink-0 w-42 rounded "
-          >
-            <div className="w-40 aspect-video rounded-lg group overflow-hidden">
+          <>
+          <div key={song.video_id} className="shrink-0 w-49 md:w-57">
+            <a
+              href={`https://www.youtube.com/watch?v=${song.video_id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-46 md:w-54 aspect-video rounded-lg group overflow-hidden block mb-2"
+            >
               <img
                 src={song.thumbnail}
                 alt="thumbnail"
                 className="object-cover transition-transform duration-300 group-hover:scale-110"
               />
-            </div>
-            
-
-              <p 
-                className="text-sm text-white line-clamp-2 mt-1"
-                title={song.title}
-              >
+            </a>
+          
+            <a href={`https://www.youtube.com/watch?v=${song.video_id}`} target="_blank" rel="noopener noreferrer" className="block w-46 md:w-54">
+              <p title={song.title} className="text-sm text-white line-clamp-2 mt-1">
                 {song.title}
               </p>
-          </a>
+            </a>
+          </div>
+          </>
         ))}
       </div>
     </div>
