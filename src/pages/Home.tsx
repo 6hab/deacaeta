4ce4c89ec04867fe187271ca1a030b7fe49bb997
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-08-23 16:27:48 +0200

    Mise à jour de Home.tsx, avec Song of the day et Recently added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,8 @@
 import { Header } from "../components/Header";
 import { SongOfTheDay } from "../components/SongOfTheDay";
 import { RecentlyAdded } from "../components/RencentlyAdded";
-import { TopViewedSongs } from "../components/TopViewedSongs";
-
-import { SongCard } from "../components/cards/SongCard";
-import { useSongs } from "../hooks/useSongs";
 
 export function Home() {
-  //const { songs, loading, error } = useSongs();
-
-  const HomeTags = [
-    // Genre
-    "Anime",
-    "Beat",
-    "Hyperpop",
-
-    // Source
-    "Cartoon",
-    "TV series",
-    "Video game",
-
-    // Format
-    "Fanmande",
-    "Instrumental",
-    "Nightcore",
-
-    // Language
-    "Japanese",
-    "English",
-    "French",
-  ];
 
   return (
     <div>
@@ -38,7 +11,7 @@ export function Home() {
       {/* Body */}
       <div className="px-5 py-5">
 
-        <div className="flex flex-col items-center md:flex-row md:items-start md:justify-center gap-6 text-white max-w-7xl mx-auto">
+        <div className="flex flex-col items-center md:flex-row md:items-start md:justify-center gap-6 max-w-7xl mx-auto">
           <div className="">
             <SongOfTheDay/>
           </div>
@@ -50,14 +23,8 @@ export function Home() {
               <RecentlyAdded/>
             </div>
 
-            <div className="">
-              <TopViewedSongs />
-            </div>
-
           </div>
       </div>
-
-
 
     </div>
   );
