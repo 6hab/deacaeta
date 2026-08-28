ad34a48221623b5d4680be8d7a940c43625ef554
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-08-28 23:28:09 +0200

    avancement de SOng of the day

--- src/components/SongOfTheDay.tsx
+++ src/components/SongOfTheDay.tsx
@@ -7,21 +7,20 @@ export function SongOfTheDay() {
   const { song, loading, error } = useSongOfTheDay();
   const [isPlaying, setIsPlaying] = useState(false);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
-  if (!song) return <p>Not found</p>;
+  if (loading) return null;
+  if (error) return console.log(error);
+  if (!song) return null;
 
   return (
-    <div className="relative rounded-2xl overflow-hidden min-h-70">
+    <div className="relative rounded-2xl overflow-hidden min-h-70 w-full max-w-165">
       <img src={song.thumbnail} alt="" 
         className="absolute inset-0 w-full h-full object-cover blur scale-110"
       />
 
       <div className="absolute inset-0  bg-black/10" />
 
-      <div className="relative flex flex-col md:flex-row items-center gap-6 p-5 mb-3">
-        <div className="flex-1 min-w-0">
-          
+      <div className="relative p-5">
+        <div className="w-full">          
           <span className="inline-block bg-amber-400/40 rounded-full px-3 py-1 text-sm mb-3">
             <div className="flex text-white items-center gap-1">
               <Sparkle size={16} className="text-amber-400"/>
@@ -29,7 +28,7 @@ export function SongOfTheDay() {
             </div>
           </span>
 
-          <p className="text-2xl font-bold text-white line-clamp mb-3">{song.title}</p>
+          <p className="text-white text-2xl font-bold  line-clamp mb-3">{song.title}</p>
 
           <div className="flex flex-wrap items-center gap-x-3 mb-3">
             {song.artists.map((artist, index) => (
@@ -62,17 +61,18 @@ export function SongOfTheDay() {
             ))}
           </div>
 
-          <div className="w-full md:w-80 aspect-video rounded-lg overflow-hidden shrink-0 relative">
+
+          <div className="relative w-full aspect-video rounded-lg overflow-hidden shrink-0">
             {isPlaying ? (
               <iframe
-                className="w-full h-full"
+                className="absolute inset-0 w-full h-full"
                 src={`https://www.youtube.com/embed/${song.video_id}?autoplay=1`}
                 title={song.title}
                 allow="autoplay; encrypted-media"
                 allowFullScreen
               />
             ) : (
-              <button onClick={() => setIsPlaying(true)} className="relative w-full h-full">
+              <button onClick={() => setIsPlaying(true)} className="absolute inset-0 w-full h-full">
                 <img 
                   src={song.thumbnail} 
                   alt="" 
