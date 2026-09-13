7e1d9ccd856e3366b55e928f27b4ab1e11975885
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-09-13 22:46:32 +0200

    feat: ajout de top10viewed.tsx avec variant par rapport au type d'affichage (list/grid)

--- src/components/Top10Viewed.tsx
+++ src/components/Top10Viewed.tsx
@@ -1,7 +1,10 @@
 import { useTopViewedSongs } from "../hooks/home/useTopViewedSongs";
 import { Link } from "react-router-dom";
+import { ChevronRight, Eye } from "lucide-react";
+import { SongCard } from "./cards/SongCard";
 
-export function Top10Viewed() {
+
+export function Top10Viewed({ variant }: { variant: "list" | "grid" }) {
   const { songs, loading, error } = useTopViewedSongs();
   const topSongs = songs.slice(0, 10);
 
@@ -13,25 +16,71 @@ export function Top10Viewed() {
   if (loading) return null;
   if (error) return null;
 
+  if (variant === "grid") {
+    return (
+      <div>
+        <div className="flex text-white items-center justify-between mb-2 px-5">
+          <div className="flex items-center gap-1">
+            <Eye size={16} />
+            Most viewed
+          </div>
+
+          <Link to={"/top"} className="text-white/70 hover:text-white inline-flex items-center gap-0.5">
+            <p>View all</p>
+            <ChevronRight size={16} />
+          </Link>
+        </div>
+
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 px-5">
+          {topSongs.map((song, index) => (
+            <div key={song.video_id} className="relative h-full">
+              <span className="absolute z-10 text-white bg-mauve-700/70 rounded-full top-2 left-2 w-7 h-7 flex items-center justify-center font-semibold">{index + 1}</span>
+              
+              <span className="absolute z-10 text-white/50 text-xs bg-black/60 rounded-full right-2 bottom-2 flex items-center px-2 py-0.5 gap-1">
+                <Eye size={12} />
+                {formatViews(song.view_count)}
+              </span>
+              <SongCard {...song} />
+            </div> 
+          ))}
+        </div>
+
+      </div>
+    )
+  }
+
   return (
     <div>
-      {topSongs.map((song) => (
+      <div className="flex text-white items-center gap-1 mb-2">
+        <Eye size={16} />
+        Most viewed
+      </div>
+      {topSongs.map((song, index) => (
         <div key={song.video_id}>
-          <Link to={`/song/${song.video_id}`}>
-            <img src={song.thumbnail} alt="thumbnail" />
-          </Link>
-          <p>{song.title}</p>
-
-          {song.artists.map((artist) => (
-            <p key={artist.artist_id}>
-              <Link to={`/artist/${artist.artist_id}`}>
-                {artist.name_original}
+          <div className="flex items-center gap-3 bg-black/50 rounded-lg mb-2 p-1">
+            <span className="text-white/50 font-semibold w-4 text-right">{index + 1}</span>
+              <Link to={`/song/${song.video_id}`} className="shrink-0">
+              <img src={song.thumbnail} alt="thumbnail" className="w-14 h-14 rounded-lg object-cover"/>
               </Link>
-            </p>
-          ))}
-          <p>{formatViews(song.view_count)}</p>
+
+              <div className="min-w-0">
+                <Link to={`/song/${song.video_id}`}>
+                  <p className="text-white/80 hover:text-white text-sm truncate">{song.title}</p>
+                </Link>
+
+                <p className="text-white/50 text-xs">{formatViews(song.view_count)}</p>
+              </div>
+          </div>
         </div>
       ))}
+
+      <div>
+        <Link to={"/top"} className="text-white/70 hover:text-white inline-flex items-center gap-0.5">
+          <p className="">View all</p>
+          <ChevronRight size={16} className="translate-y-0"/>
+        </Link>
+        
+      </div>
     </div>
   );
 }
