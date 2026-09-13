42e28e2353092301e001e352dd1ec0e6a32b9f30
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-09-13 22:51:57 +0200

    feat:  ajout de la sécurité optionelle pour l'artiste (ex: artiste manquant) + correction de bug des cards songs n'ayant pas la même taille (top10viewed)

--- src/components/cards/SongCard.tsx
+++ src/components/cards/SongCard.tsx
@@ -20,18 +20,21 @@ export interface SongCardProps {
 
 export function SongCard(song: Readonly<SongCardProps>) {
   return (
-    <div className="border border-white/12 rounded-xl overflow-hidden cursor-pointer hover:border-white transition-colors bg-white/8 backdrop-blur">
-      <img src={song.thumbnail}
-        className="w-full object-cover aspect-video"
-      />
+    <div className="h-full flex flex-col border border-white/12 rounded-xl overflow-hidden hover:border-white transition-colors bg-white/8 backdrop-blur">
+      <Link to={`/song/${song.video_id}`}>
+        <img src={song.thumbnail}
+        className="w-full object-cover aspect-video cursor-pointer"
+        />
+      </Link>
+      
       <div className="px-3 py-2">
 
-        <p className="text-sm font-medium text-neutral-100 line-clamp">
+        <p className="text-sm font-medium text-neutral-100 line-clamp-2">
           {song.title}
         </p>
 
         <p className="text-xs text-neutral-400 mt-0.5">
-          {song.artists.map((artist, index) => (
+          {song.artists?.map((artist, index) => (
             <span key={artist.artist_id}>
               {index !== 0 && <span>, </span>}
               <Link to={`/artist/${artist.artist_id}/${slugify(artist.stage_name)}`} className="inline-flex hover:underline">{artist.stage_name}</Link>
@@ -42,12 +45,12 @@ export function SongCard(song: Readonly<SongCardProps>) {
         <p className="text-xs italic text-neutral-400 mt-1">
           {song.video_published_at.slice(0, 4)}
         </p>
-      </div>
 
-      {/*<p>{song.tags.map((tag) => (
-                <span key={tag}>{tag} </span>
-            ))}</p> {" "} 
-      <br /> */}
+        <p className="text-xs italic text-neutral-300 mt-1">
+          {song.uploader}
+        </p>
+      </div>
     </div>
+
   );
 }
