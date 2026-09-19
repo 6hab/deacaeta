5034870b290ac0297f7015598829cdb6e592cd63
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-09-19 18:08:17 +0200

    feat: affichage du top de Home en permanence sur tout type d'écran

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,26 +13,29 @@ export function Home() {
       <Header />
       {/* Body */}
       <div className="px-5 py-5">
-        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-[1fr_320px] gap-5 items-start">
-          <div className="flex flex-col items-center md:items-stretch gap-5 md:pr-10 md:pl-10 min-w-0">
+
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-5 items-start">
+          <div className="flex flex-col items-center md:items-stretch gap-5 w-full md:mr- md:ml-10 min-w-0">
           <SongOfTheDay />
           
           <div className="w-full hidden max-md:block">
-            <RecentlyAdded variant="row"/>
+            <RecentlyAdded variant="row" limit={15} />
           </div>
           
           <TagRow limit={15} tag="Anime"/>
           <TagRow limit={15} tag="Video game"/>
           <TagRow limit={15} tag="Beat"/>
-
-          <div className="w-full md:hidden">
-            <Top10Viewed variant="grid" />
-          </div>
         </div>
-        <div className="hidden md:block">
+
+        <div className="hidden md:w-80 md:block w-full">
           <HomeSidePanel />
         </div>
       </div>
+
+      <div className="max-w-7xl mx-auto mt-10 px-2">
+        <Top10Viewed variant="grid" />
+      </div>
+
     </div>
     </div>
   );
